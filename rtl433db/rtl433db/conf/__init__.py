5243055c19ec8fdcfb4b4d4ce1fc957d8a0b1bb0
gwvsol <gwvsol@gmail.com>
2023-01-25 15:48:42 +0300

    создание настроек приложения

---
+++ rtl433db/rtl433db/conf/__init__.py
@@ -0,0 +1,7 @@
+from rtl433db.conf.conf import postgresql_url,\
+                               rtl433_command
+
+__all__ = (
+    'postgresql_url',
+    'rtl433_command',
+)
---
+++ rtl433db/rtl433db/conf/conf.py
@@ -0,0 +1,67 @@
+import sys
+import psycopg2
+import sqlalchemy
+from pathlib import Path
+from shutil import copy as shutil_copy
+from configparser import ConfigParser
+from sqlalchemy.exc import ProgrammingError, \
+                           OperationalError
+
+from rtl433db.log import logging as log
+
+
+base_dir = Path(__file__).parent.parent.parent
+conf_path = base_dir.parent
+config_file = 'rtl433db.conf'
+
+
+if conf_path.as_posix() in sys.executable:
+    config = base_dir.parent/config_file
+else:
+    config = Path(sys.executable).parent
+    config = config/config_file
+    if not config.exists():
+        confbase = base_dir/config_file
+        log.info(f'confbase => {confbase.as_posix()}')
+        log.info(f'config => {config.as_posix()}')
+        shutil_copy(src=confbase.as_posix(), dst=config.as_posix())
+
+conf = ConfigParser()
+conf.read(config)
+
+# =====================================================
+#
+_postgresql = conf['POSTGRESQL']
+postgresql_host = _postgresql.get('postgresql_host')
+postgresql_port = _postgresql.getint('postgresql_port')
+postgresql_user = _postgresql.get('postgresql_user')
+postgresql_password = _postgresql.get('postgresql_password')
+postgresql_dbname = _postgresql.get('postgresql_dbname')
+_postgresql_url = 'postgresql+psycopg2://{}:{}@{}:{}'.format(
+        postgresql_user, postgresql_password,
+        postgresql_host, postgresql_port
+    )
+postgresql_url = '{}/{}'.format(_postgresql_url, postgresql_dbname)
+
+
+try:
+    with sqlalchemy.create_engine(
+        postgresql_url,
+        isolation_level='AUTOCOMMIT'
+    ).connect() as connection:
+        log.info(f"DATABASE: {postgresql_dbname} => exists")
+        log.info(f"POSTGRESQL => {postgresql_url}")
+except (psycopg2.OperationalError, OperationalError):
+    try:
+        with sqlalchemy.create_engine(
+            _postgresql_url,
+            isolation_level='AUTOCOMMIT'
+        ).connect() as connection:
+            connection.execute(f"CREATE DATABASE {postgresql_dbname};")
+            log.info(f"DATABASE: {postgresql_dbname} => created")
+            log.info(f"POSTGRESQL => {postgresql_url}")
+    except (ProgrammingError, OperationalError) as err:
+        log.error(err)
+
+
+rtl433_command = 'rtl_433 -F json'
