44a61678e40c8377fa58d60c8a5238ab2d080979
gwvsol <gwvsol@gmail.com>
2023-01-31 16:50:14 +0300

    создание метода add_data для записи в базу данных

--- rtl433db/rtl433db/rtl433/rtl433.py
+++ rtl433db/rtl433db/rtl433/rtl433.py
@@ -3,9 +3,40 @@ import json
 from json.decoder import JSONDecodeError
 from subprocess import Popen, PIPE
 from multiprocessing import Process, Queue
+from sqlalchemy.orm import Session
 
 from rtl433db.conf import rtl433_command as command
 from rtl433db.log import logging as log
+
+from rtl433db.db import Base, Sensors, Temperature, engine
+
+
+def add_data(data: dict):
+    """ Запись данных в базу данных """
+
+    model = data.get('model', None)
+    datetime = data.get('time', None)
+    temp = data.get('temperature_C', None)
+    sensor_id = data.get('id', None)
+
+    with Session(autoflush=False, bind=engine) as db:
+        sensor = db.query(Sensors).filter(Sensors.model == model).first()
+
+        if sensor:
+            sensor_id = sensor.id
+            temperature = Temperature(sensor_id=sensor.id,
+                                      temperature=temp,
+                                      datetime=datetime)
+            db.add(temperature)
+            db.commit()
+        else:
+            if model and isinstance(sensor_id, (int, str)):
+                new_sensor = dict(model=model, sensor_id=str(sensor_id))
+                if datetime:
+                    new_sensor.update(datetime=datetime)
+                new_sensor = Sensors(**new_sensor)
+                db.add(new_sensor)
+                db.commit()
 
 
 def rtl433(queue: Queue, proc: Popen):
@@ -37,6 +68,9 @@ def main():
                       args=(queue, proc),
                       name='rtl433 process')
 
+    # создаем таблицы
+    Base.metadata.create_all(bind=engine)
+
     try:
         process.start()
         log.info(" => start")
@@ -46,6 +80,7 @@ def main():
                 log.info(f"<= {data}")
                 if data == "JSONDecodeError":
                     break
+                add_data(data)
             time.sleep(1)
         proc.kill()
         proc.communicate()
