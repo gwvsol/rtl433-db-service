f2845eb7c6622c0ac6c03d7a2601cb77f75cf632
gwvsol <gwvsol@gmail.com>
2023-02-02 13:24:51 +0300

    создание декоратора format_data_temp и подключение weatherapi

--- rtl433db/rtl433db/rtl433/rtl433.py
+++ rtl433db/rtl433db/rtl433/rtl433.py
@@ -1,34 +1,52 @@
 import time
 import json
-from json.decoder import JSONDecodeError
+from functools import wraps
 from subprocess import Popen, PIPE
-from multiprocessing import Process, Queue
 from sqlalchemy.orm import Session
+from json.decoder import JSONDecodeError
+from multiprocessing import Process, Queue
+from apscheduler.schedulers.background import BackgroundScheduler
 
 from rtl433db.conf import rtl433_command as command
+from rtl433db.conf import WeatherApiConf as weathew_conf
 from rtl433db.log import logging as log
-
 from rtl433db.db import Base, Sensors, Temperature, engine
+from rtl433db.weather import weatherapi
 
 
-def add_data(data: dict):
+def format_data_temp(fun):
+    """ Декоратор для форматирования данных """
+    @wraps(fun)
+    def decorator(*args, **kwargs):
+        data = kwargs.pop('data', dict())
+        kwargs.update(model=data.get('model', None),
+                      datetime=data.get('time', None),
+                      temp=data.get('temperature_C', None),
+                      sensor_id=data.get('id', None))
+        return fun(*args, **kwargs)
+    return decorator
+
+
+@format_data_temp
+def add_data(model: str = None,
+             datetime: str = None,
+             temp: str = None,
+             sensor_id: str = None,
+             data: dict = dict()):
     """ Запись данных в базу данных """
 
-    model = data.get('model', None)
-    datetime = data.get('time', None)
-    temp = data.get('temperature_C', None)
-    sensor_id = data.get('id', None)
-
     with Session(autoflush=False, bind=engine) as db:
+
         sensor = db.query(Sensors).filter(Sensors.model == model).first()
 
-        if sensor and isinstance(temp, int):
+        if sensor and isinstance(temp, (int, float)):
             sensor_id = sensor.id
             temperature = Temperature(sensor_id=sensor.id,
                                       temperature=temp,
                                       datetime=datetime)
             db.add(temperature)
             db.commit()
+
         elif sensor is None:
             if model and isinstance(sensor_id, (int, str)):
                 new_sensor = dict(model=model, sensor_id=str(sensor_id))
@@ -37,9 +55,9 @@ def add_data(data: dict):
                 new_sensor = Sensors(**new_sensor)
                 db.add(new_sensor)
                 db.commit()
+
         else:
             pass
-            
 
 
 def rtl433(queue: Queue, proc: Popen):
@@ -64,12 +82,20 @@ def rtl433(queue: Queue, proc: Popen):
 def main():
     """ Старт приложения rtl_433 """
 
+    scheduler = BackgroundScheduler()
+    log.getLogger('apscheduler').propagate = False
+
     queue = Queue()
     proc = Popen(command.split(), stdout=PIPE)
 
     process = Process(target=rtl433,
                       args=(queue, proc),
                       name='rtl433 process')
+
+    scheduler.add_job(weatherapi,
+                      'interval',
+                      seconds=weathew_conf.interval)
+    scheduler.start()
 
     # создаем таблицы
     Base.metadata.create_all(bind=engine)
@@ -83,13 +109,14 @@ def main():
                 log.info(f"<= {data}")
                 if data == "JSONDecodeError":
                     break
-                add_data(data)
+                add_data(data=data)
             time.sleep(1)
         proc.kill()
         proc.communicate()
-        process.join() # 
+        process.join()
         log.error("TUNER DVB-T/T2/C FM & DAB => NOT FOUND => STOP")
-    except KeyboardInterrupt:
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
         proc.kill()
         proc.communicate()
         process.join()
