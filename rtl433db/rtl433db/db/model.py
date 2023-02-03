a4e5fe60880841ad0f9317bdec8d4a50a6a1fe99
gwvsol <gwvsol@gmail.com>
2023-02-03 14:55:01 +0300

    создание модели таблиц WeatherLocation и Weather

--- rtl433db/rtl433db/db/model.py
+++ rtl433db/rtl433db/db/model.py
@@ -34,3 +34,42 @@ class Temperature(Base):
     temperature = Column(Float, nullable=False)
     datetime = Column(DateTime(timezone=True),
                       default=datetime.now())
+
+
+class WeatherLocation(Base):
+
+    location = Column(String, nullable=False, index=True)
+    country = Column(String, nullable=False, index=True)
+    lat = Column(Float)
+    lon = Column(Float)
+    datetime = Column(DateTime(timezone=True),
+                      default=datetime.now())
+
+
+class Weather(Base):
+
+    location = Column(Integer,
+                      ForeignKey("weatherlocation.id"),
+                      nullable=False, index=True)
+    # Температура (в градусах Цельсия)
+    temp_c = Column(Float, nullable=False)
+    # Температура по ощущениям (в градусах Цельсия)
+    temp_feelslike_c = Column(Float)
+    # Скорость ветра в км/час
+    wind_kph = Column(Float)
+    # Направление ветра в градусах
+    wind_degree = Column(Integer)
+    # Направление ветра в виде компаса с 16 точками
+    wind_dir = Column(String)
+    # Давление в миллибарах (mmHg value = mbar value x 0.750062)
+    pressure_mb = Column(Float)
+    # Давление в мм ртутного столба
+    pressure_mmhg = Column(Float)
+    # Количество осадков в мм
+    precip_mm = Column(Integer)
+    # Влажность в %
+    humidity = Column(Integer)
+    # Порывы ветра в км/час
+    gust_kph = Column(Float)
+    datetime = Column(DateTime(timezone=True),
+                      default=datetime.now())
