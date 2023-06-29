e43886aa531ec5982e3cce93e19f75738a190efd
gwvsol <gwvsol@gmail.com>
2023-06-29 17:45:09 +0300

    обновление help приложения

--- rtl433db/rtl433db/cli/help.py
+++ rtl433db/rtl433db/cli/help.py
@@ -2,6 +2,4 @@
 class ClickHelp:
     #
     help: str = "Справка по работе с приложением"
-    rtl433db: str = "START => RTL433DB"
-    #
-    saveconf: str = "SAVE => .env -> rtl433db.conf"
+    run: str = "=> Запуск приложения RTL433DB"
