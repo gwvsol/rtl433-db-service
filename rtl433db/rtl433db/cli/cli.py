2950b6ad70fa24831ffadca05673988aedfd1770
gwvsol <gwvsol@gmail.com>
2023-06-29 17:44:47 +0300

    обновление cli приложения

--- rtl433db/rtl433db/cli/cli.py
+++ rtl433db/rtl433db/cli/cli.py
@@ -7,23 +7,8 @@ def main() -> None:
     return
 
 
-@main.command(help=cli.rtl433db)
-@click.option("-s", "--saveconf",
-              show_default=False,
-              default=False,
-              help=cli.saveconf)
-def start(saveconf: bool):
-    if saveconf:
-        from rtl433db.saveconf import saveconfig
-        click.echo("\n\033[1;96m {}\033[00m\n" .format(cli.saveconf))
-        saveconfig()
-    click.echo("\n\033[1;96m {}\033[00m\n" .format(cli.rtl433db))
+@main.command(help=cli.run)
+def run():
+    click.echo("\n\033[1;96m {}\033[00m\n" .format(cli.run))
     from rtl433db.rtl433 import main
     main()
-
-
-@main.command(help=cli.saveconf)
-def saveconf():
-    click.echo("\n\033[1;96m {}\033[00m\n" .format(cli.saveconf))
-    from rtl433db.saveconf import saveconfig
-    saveconfig()
