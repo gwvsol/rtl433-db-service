3680b77da37400a783d4b28c410444ce4756f272
gwvsol <gwvsol@gmail.com>
2023-06-30 14:23:51 +0300

    обновление cli приложения

--- rtl433db/rtl433db/cli/cli.py
+++ rtl433db/rtl433db/cli/cli.py
@@ -10,5 +10,5 @@ def main() -> None:
 @main.command(help=cli.run)
 def run():
     click.echo("\n\033[1;96m {}\033[00m\n" .format(cli.run))
-    from rtl433db.rtl433 import main
-    main()
+    from rtl433db.app import run_app
+    run_app()
