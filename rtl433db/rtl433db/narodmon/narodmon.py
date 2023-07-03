ebc7b17381bc85e4490d96a7b11d9c872ea5aafc
Michail Fedorov <gwvsol@gmail.com>
2023-07-03 22:59:41 +0300

    создан метод format_data

--- rtl433db/rtl433db/narodmon/narodmon.py
+++ rtl433db/rtl433db/narodmon/narodmon.py
@@ -1,11 +1,18 @@
 # import socket
 
-# from rtl433db.log import logging as log
-# from rtl433db.conf import NarodMonConf as narodmon_conf
+from rtl433db.log import logging as log
+from rtl433db.conf import Rtl433Conf as rtl433_conf
+from rtl433db.conf import NarodMonConf as narodmon_conf
 
 
-# def format_data(data: tuple) -> bytes:
-#     temperature, humidity, datetime, model = data
+def format_data(data: tuple) -> bytes:
+    login = narodmon_conf.login
+    mac = narodmon_conf.sensor
+    name = narodmon_conf.sensor_model
+    latitude = narodmon_conf.latitude
+    longitude = narodmon_conf.longitude
+    
+    temperature, humidity, datetime, model = data
 
 
 # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
